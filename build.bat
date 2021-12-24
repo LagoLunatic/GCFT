1194eeac38be5d4ae244e034fff0a9858da319f8
LagoLunatic <LagoLunatic@users.noreply.github.com>
2021-12-24 18:48:32 -0500

    Update build script to Python 3.9 too

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 
-py -3.8-32 -m PyInstaller gcft.spec
-py -3.8-32 build.py
+py -3.9-32 -m PyInstaller gcft.spec
+py -3.9-32 build.py
 
-py -3.8 -m PyInstaller gcft.spec
-py -3.8 build.py
+py -3.9 -m PyInstaller gcft.spec
+py -3.9 build.py
