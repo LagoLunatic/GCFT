8aa691f414d8bafd62ca6bae47fc994f2e9b805d
LagoLunatic <LagoLunatic@users.noreply.github.com>
2024-06-20 19:49:46 -0400

    Support dark mode, use Qt Fusion style on Windows

--- gcft.py
+++ gcft.py
@@ -28,8 +28,43 @@ except ImportError:
     # Versions of Windows before Windows 7 don't support SetCurrentProcessExplicitAppUserModelID, so just swallow the error.
     pass
 
+def get_dark_mode_palette(app) :
+  from PySide6.QtGui import QPalette, QColor
+  pal = app.palette()
+
+  pal.setColor(QPalette.ColorRole.Window, QColor("#353535"))
+  pal.setColor(QPalette.ColorRole.WindowText, QColor("#D9D9D9"))
+  pal.setColor(QPalette.ColorRole.Base, QColor("#2A2A2A"))
+  pal.setColor(QPalette.ColorRole.AlternateBase, QColor("#424242"))
+  pal.setColor(QPalette.ColorRole.ToolTipBase, QColor("#353535"))
+  pal.setColor(QPalette.ColorRole.ToolTipText, QColor("#D9D9D9"))
+  pal.setColor(QPalette.ColorRole.Text, QColor("#D9D9D9"))
+  pal.setColor(QPalette.ColorRole.Dark, QColor("#232323"))
+  pal.setColor(QPalette.ColorRole.Shadow, QColor("#141414"))
+  pal.setColor(QPalette.ColorRole.Button, QColor("#353535"))
+  pal.setColor(QPalette.ColorRole.ButtonText, QColor("#D9D9D9"))
+  pal.setColor(QPalette.ColorRole.BrightText, QColor("#D90000"))
+  pal.setColor(QPalette.ColorRole.Link, QColor("#2A82DA"))
+  pal.setColor(QPalette.ColorRole.Highlight, QColor("#2A82DA"))
+  pal.setColor(QPalette.ColorRole.HighlightedText, QColor("#D9D9D9"))
+
+  pal.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole.WindowText, QColor("#7F7F7F"))
+  pal.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole.Text, QColor("#7F7F7F"))
+  pal.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole.ButtonText, QColor("#7F7F7F"))
+  pal.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole.Highlight, QColor("#505050"))
+  pal.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole.HighlightedText, QColor("#7F7F7F"))
+
+  return pal
+
 if __name__ == "__main__":
   qApp = QApplication(sys.argv)
+  
+  # Use the Qt Fusion style on all platforms for consistency to avoid parts of the UI breaking on certain OSes.
+  qApp.setStyle("Fusion")
+  
+  qt_version_tuple = (6, 6, 0)#tuple(int(num) for num in QT_VERSION.split("."))
+  if qt_version_tuple >= (6, 5, 0) and qApp.styleHints().colorScheme() == Qt.ColorScheme.Dark:
+    qApp.setPalette(get_dark_mode_palette(qApp))
   
   def show_unhandled_exception(excepttype, exception, tb):
     sys.__excepthook__(excepttype, exception, tb)
